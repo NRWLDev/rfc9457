c0978c9abd7719a546d03bbc72de37b1b08e7edc
Daniel Edgy Edgecombe <edgy.edgemond@gmail.com>
2024-05-15 18:24:55 +0100

    Update __init__.py

--- src/rfc9457/__init__.py
+++ src/rfc9457/__init__.py
@@ -1,1 +1,103 @@
 
+"""Implement RFC9547 compatible exceptions.
+
+https://www.rfc-editor.org/rfc/rfc9457.html
+"""
+
+from __future__ import annotations
+
+import re
+import typing as t
+from warnings import warn
+
+CONVERT_RE = re.compile(r"(?<!^)(?=[A-Z])")
+
+
+class Problem(Exception):  # noqa: N818
+    """
+    A base exception designed to support all API error handling.
+    All exceptions should inherit from this or a subclass of it (depending on the usage),
+    this will allow all apps and libraries to maintain a common exception chain.
+    """
+
+    def __init__(
+        self: t.Self,
+        title: str,
+        type_: str | None = None,
+        details: str | None = None,
+        status: int = 500,
+        **kwargs,
+    ) -> None:
+        super().__init__(title)
+        self._type = type_
+        self.title = title
+        self.details = details
+        self.status = status
+        self.extras = kwargs
+
+    @property
+    def type(self: t.Self) -> str:
+        type_ = "".join(self.__class__.__name__.rsplit("Error", 1))
+        type_ = CONVERT_RE.sub("-", type_).lower()
+        return self._type if self._type else type_
+
+    def marshal(self: t.Self, *, strip_debug: bool = False) -> dict[str, t.Any]:
+        """Generate a JSON compatible representation.
+
+        Args:
+        ----
+            strip_debug: If true, remove anything that is not title/type.
+        """
+        ret = {
+            "type": self.type,
+            "title": self.title,
+            "status": self.status,
+        }
+
+        if self.details:
+            ret["details"] = self.details
+
+        for k, v in self.extras.items():
+            ret[k] = v
+
+        if strip_debug:
+            ret = {k: v for k, v in ret.items() if k in ["type", "title", "status"]}
+
+        return ret
+
+
+class StatusProblem(Problem):
+    code = None
+    type_ = None
+    title = "Base http exception."
+    status = 500
+
+    def __init__(self: t.Self, details: str | None = None, **kwargs) -> None:
+        super().__init__(self.title, type_=self.type_ or self.code, details=details, status=self.status, **kwargs)
+
+
+class ServerProblem(StatusProblem): ...
+
+
+class BadRequestProblem(StatusProblem):
+    status = 400
+
+
+class UnauthorisedProblem(StatusProblem):
+    status = 401
+
+
+class ForbiddenProblem(StatusProblem):
+    status = 403
+
+
+class NotFoundProblem(StatusProblem):
+    status = 404
+
+
+class ConflictProblem(StatusProblem):
+    status = 409
+
+
+class UnprocessableProblem(StatusProblem):
+    status = 422
