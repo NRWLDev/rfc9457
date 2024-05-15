111030dad967c75c8c09f29997e2b99ea4bce431
Daniel Edgy Edgecombe <edgy.edgemond@gmail.com>
2024-05-15 18:53:53 +0100

    Create test_rfc9457.py

---
+++ tests/test_rfc9457.py
@@ -0,0 +1,39 @@
+import pytest
+
+import rfc9457 as error
+
+
+class NotFoundError(error.NotFoundProblem):
+    title = "a 404 message"
+
+
+class InvalidAuthError(error.UnauthorisedProblem):
+    title = "a 401 message"
+
+
+class BadRequestError(error.BadRequestProblem):
+    title = "a 400 message"
+
+
+class ServerExceptionError(error.ServerProblem):
+    title = "a 500 message"
+
+
+@pytest.mark.parametrize(
+    ("exc", "type_"),
+    [
+        (NotFoundError, "not-found"),
+        (InvalidAuthError, "invalid-auth"),
+        (BadRequestError, "bad-request"),
+        (ServerExceptionError, "server-exception"),
+    ],
+)
+def test_marshal(exc, type_):
+    e = exc("details")
+
+    assert e.marshal() == {
+        "type": type_,
+        "title": e.title,
+        "details": "details",
+        "status": e.status,
+    }
